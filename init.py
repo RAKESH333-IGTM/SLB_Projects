9d2a63e34f30ae8dea1f5563884ec6b3049b362d
Rakesh Choutpally <rakesh.choutpally@slbsolution.com>
2025-10-30 21:08:04 +0530

    add modified file to the local repo

--- init.py
+++ init.py
@@ -2,5 +2,12 @@ name = input("Enter your name: ")
 
 if name:
     print(f"Hello, {name}! Nice to meet you.")
+    # modified code
+    print(f"hey {name} i hope you are doing well")
 else:
     print("You didn’t enter a name!")
+
+# modified code
+    print("hey {name} i hope you are doing well")
+
+
